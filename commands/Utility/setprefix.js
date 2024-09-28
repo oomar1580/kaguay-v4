3b9e26b7331353a9e8c00ef3996bd46d6977531b
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-09-28 15:32:17 +0100

    Update setprefix.js

--- commands/Utility/setprefix.js
+++ commands/Utility/setprefix.js
@@ -1,62 +1,32 @@
-import axios from 'axios';
-import fs from 'fs-extra';
-import jimp from 'jimp';
+import fs from "fs";
 
-async () => {
-    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-    const dirMaterial = process.cwd() + `/cache/`;
-    if (!existsSync(dirMaterial)) mkdirSync(dirMaterial, { recursive: true });
-}
+export default {
+  name: "حقيقة&جرأة",
+  author: "Hussein Yacoubi",
+  role: "member",
+  description: "يُطرح على المستخدمين سؤال حقيقة أو تحدي (جرأة) بشكل عشوائي.",
+  async execute({ api, args, event }) {
+    const [arg1] = args;
 
-async function generateWantedImage(uid) {
-    const pathImg = process.cwd() + "/cache/wanted.png";
-    const pathAva = process.cwd() + "/cache/avt.png";
-
-    const Avatar = (
-        await axios.get(
-            `https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
-            { responseType: "arraybuffer" }
-        )
-    ).data;
-
-    fs.writeFileSync(pathAva, Buffer.from(Avatar, "utf-8"));
-
-    const getWanted = (
-        await axios.get(`https://i.imgur.com/2FarLuj.jpg`, {
-            responseType: "arraybuffer",
-        })
-    ).data;
-
-    fs.writeFileSync(pathImg, Buffer.from(getWanted, "utf-8"));
-
-    const baseImage = await jimp.read(pathImg);
-    const baseAva = await jimp.read(pathAva);
-
-    baseImage.resize(600, 800);
-    baseImage.composite(baseAva.resize(345, 300), 120, 200);
-
-    await baseImage.writeAsync(pathImg);
-
-    fs.removeSync(pathAva);
-
-    return pathImg;
-}
-
-const exampleCommand = {
-    name: "مطلوب",
-    author: "حسين يعقوبي",
-    role: "member",
-    description: "صورتك على ملصق المطلوبين",
-    execute: async ({ api, event, args }) => {
-        const uid = args.length > 0 ? Object.keys(event.mentions)[0] : event.senderID;
-        const imagePath = await generateWantedImage(uid);
-        api.sendMessage({ attachment: fs.createReadStream(imagePath) }, event.threadID);
-    },
-    onReply: async ({ api, event, reply }) => {
-        const uid = event?.messageReply?.senderID || (Object.keys(event.mentions).length > 0 ? Object.keys(event.mentions)[0] : event.senderID);
-        const imagePath = await generateWantedImage(uid);
-        api.sendMessage({ attachment: fs.createReadStream(imagePath) }, event.threadID);
+    if (!arg1) {
+      api.sendMessage("إذا كنت تريد لعب حقيقة أم جرأة، يرجى استخدام 'حقيقة&جرأة حقيقة' أو 'حقيقة&جرأة جرأة'.", event.threadID || event.senderID);
+      return;
     }
-};
 
-export default exampleCommand;
+    if (arg1.toLowerCase() === 'حقيقة') {
+      const truthQuestions = JSON.parse(fs.readFileSync(`TRUTHQN.json`));
+      const randomIndex = Math.floor(Math.random() * truthQuestions.length);
+      const randomQuestion = truthQuestions[randomIndex];
+
+      api.sendMessage(`هاهو سؤال الحقيقة: \n${randomQuestion}`, event.threadID || event.senderID);
+    } else if (arg1.toLowerCase() === 'جرأة') {
+      const dareChallenges = JSON.parse(fs.readFileSync(`DAREQN.json`));
+      const randomIndex = Math.floor(Math.random() * dareChallenges.length);
+      const randomChallenge = dareChallenges[randomIndex];
+
+      api.sendMessage(`هاهو التحدي الذي يجب أن تقوم به: \n${randomChallenge}`, event.threadID || event.senderID);
+    } else {
+      api.sendMessage("استخدام غير صالح، يرجى استخدام 'حقيقة&جرأة حقيقة' لسؤال الحقيقة أو 'حقيقة&جرأة جرأة' لسؤال التحدي.", event.threadID || event.senderID);
+    }
+  }
+};
