212beeb859a226f108ef944c269a2ad5a2618072
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-09-28 08:05:00 +0100

    Update setprefix.js

--- commands/Utility/setprefix.js
+++ commands/Utility/setprefix.js
@@ -1,32 +1,62 @@
-import jimp from "jimp";
+import axios from 'axios';
+import fs from 'fs-extra';
+import jimp from 'jimp';
 
-export default {
-  name: "setprefix",
-  author: "Arjhil Dacayanan",
-  cooldowns: 60,
-  description: "Set the group prefix",
-  role: "owner",
-  aliases: ["prefix"],
-  execute: async ({ event, Threads, args }) => {
-    if (!event.isGroup) {
-      return kaguya.reply("This command can only be used in groups!");
+async () => {
+    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+    const dirMaterial = process.cwd() + `/cache/`;
+    if (!existsSync(dirMaterial)) mkdirSync(dirMaterial, { recursive: true });
+}
+
+async function generateWantedImage(uid) {
+    const pathImg = process.cwd() + "/cache/wanted.png";
+    const pathAva = process.cwd() + "/cache/avt.png";
+
+    const Avatar = (
+        await axios.get(
+            `https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
+            { responseType: "arraybuffer" }
+        )
+    ).data;
+
+    fs.writeFileSync(pathAva, Buffer.from(Avatar, "utf-8"));
+
+    const getWanted = (
+        await axios.get(`https://i.imgur.com/2FarLuj.jpg`, {
+            responseType: "arraybuffer",
+        })
+    ).data;
+
+    fs.writeFileSync(pathImg, Buffer.from(getWanted, "utf-8"));
+
+    const baseImage = await jimp.read(pathImg);
+    const baseAva = await jimp.read(pathAva);
+
+    baseImage.resize(600, 800);
+    baseImage.composite(baseAva.resize(345, 300), 120, 200);
+
+    await baseImage.writeAsync(pathImg);
+
+    fs.removeSync(pathAva);
+
+    return pathImg;
+}
+
+const exampleCommand = {
+    name: "مطلوب",
+    author: "حسين يعقوبي",
+    role: "member",
+    description: "صورتك على ملصق المطلوبين",
+    execute: async ({ api, event, args }) => {
+        const uid = args.length > 0 ? Object.keys(event.mentions)[0] : event.senderID;
+        const imagePath = await generateWantedImage(uid);
+        api.sendMessage({ attachment: fs.createReadStream(imagePath) }, event.threadID);
+    },
+    onReply: async ({ api, event, reply }) => {
+        const uid = event?.messageReply?.senderID || (Object.keys(event.mentions).length > 0 ? Object.keys(event.mentions)[0] : event.senderID);
+        const imagePath = await generateWantedImage(uid);
+        api.sendMessage({ attachment: fs.createReadStream(imagePath) }, event.threadID);
     }
-
-    const getThread = await Threads.find(event.threadID);
-
-    const responses = {
-      true: () => {
-        if (args[0]) {
-          Threads.update(event.threadID, { prefix: args[0] }).then(() => {
-            kaguya.reply("Successfully changed your group's prefix to: " + args[0]);
-          });
-        } else {
-          kaguya.reply(`The current prefix for your group is: ${getThread.data?.data?.prefix || "not set"}`);
-        }
-      },
-      false: () => kaguya.reply("No information found for your group in the database"),
-    };
-
-    responses[getThread?.status || false]();
-  },
 };
+
+export default exampleCommand;
