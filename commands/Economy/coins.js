f783889bc6065a8f555c122016b6de7836781a10
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-09-28 06:34:04 +0100

    Update coins.js

--- commands/Economy/coins.js
+++ commands/Economy/coins.js
@@ -1,35 +1,35 @@
 export default {
-  name: "coins",
-  author: "Arjhil Dacayanan",
+  name: "رصيدي",
+  author: "Hussein Yacoubi",
   cooldowns: 5,
-  description: "Manage coins in the account!",
+  description: "يعرض رصيدك المالي أو رصيد شخص آخر!",
   role: "member",
-  aliases: ["money", "economy"],
+  aliases:["رصيد"],
   async execute({ api, event, Economy, args }) {
     try {
-      const [action, amount] = args;
+      let targetID = event.senderID; // افتراضيًا، نبدأ برصيد المستخدم الحالي
 
-      if (["add", "remove"].includes(action) && !global.client.config.ADMIN_IDS.includes(event.senderID)) {
-        return api.sendMessage("You don't have the permission to use this command", event.threadID);
+      // التحقق مما إذا كان هناك رد على شخص آخر
+      if (event.messageReply) {
+        targetID = event.messageReply.senderID;
+      } else if (event.mentions && Object.keys(event.mentions).length > 0) {
+        // التحقق مما إذا تم ذكر شخص آخر في الرسالة
+        targetID = Object.keys(event.mentions)[0];
       }
 
-      const actions = {
-        add: Economy.increase,
-        remove: Economy.decrease,
-        pay: Economy.pay,
-        default: async () => {
-          const balance = await Economy.getBalance(event.senderID);
-          return api.sendMessage(`Your current coins balance is: ${balance.data}\n\n${global.client.config.ADMIN_IDS.includes(event.senderID) ? `[Because you are an admin, you will see this message]\n!coins add <amount> <@tag> to add coins for the user\n!coins remove <amount> <@tag> to deduct coins from the user` : ""}`, event.threadID);
-        },
-      };
+      const userInfo = await api.getUserInfo(targetID);
+      const userName = userInfo[targetID].name;
 
-      const actionFunction = actions[action] || actions["default"];
-      const result = await actionFunction(parseInt(amount), event?.messageReply?.senderID || Object.keys(event.mentions)[0]);
+      const balance = await Economy.getBalance(targetID);
+      const money = balance.data; // رصيد المستخدم
 
-      return api.sendMessage(result.data, event.threadID);
+      let replyMessage = `رصيد ${targetID === event.senderID ? "ك" : ` ${userName}`} الحالي هو: ${money} دولار`;
+
+      // قم بإرسال الرد
+      return api.sendMessage(replyMessage, event.threadID);
     } catch (error) {
       console.error(error);
-      return api.sendMessage("An error occurred, please try again later!", event.threadID);
+      return api.sendMessage("❌ | لقد حدث خطأ، رجاءً أعد المحاولة لاحقًا!", event.threadID);
     }
   },
 };
