9f5dd0d8bd0da948822c2ed12be6b5bf0c0a2bba
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2024-10-28 01:59:49 +0100

    Update Avatar.js

--- commands/AAIBox/Avatar.js
+++ commands/AAIBox/Avatar.js
@@ -6,21 +6,21 @@ async function avatarAlert({ api, event, args }) {
   try {
     const { threadID, messageID } = event;
 
-    // التحقق من أن المستخدم أدخل 4 قيم
-    if (args.length !== 4) {
-      return api.sendMessage("⚠️ | يرجى إدخال 4 معلومات بالترتيب: id, bgname, signature, color.\nمثال: 1 Joshua 'Joshua Sy' blue", threadID, messageID);
+    // التحقق من أن المستخدم أدخل 5 قيم فقط (name, subname, id, colorname, colorsub)
+    if (args.length !== 5) {
+      return api.sendMessage("⚠️ | يرجى إدخال 5 معلومات بالترتيب: name, subname, id, colorname, colorsub.\nمثال: John Doe 123 blue red", threadID, messageID);
     }
 
     // استخلاص القيم من المدخلات
-    const [id, bgname, signature, color] = args;
+    const [name, subname, id, colorname, colorsub] = args;
     const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
-    const imagePath = path.join(process.cwd(), 'cache', `${timestamp}_avatar.png`);
+    const imagePath = path.join(process.cwd(), 'cache', `${timestamp}_fbcover.png`);
 
     // إضافة رد فعل لإعلام المستخدم بأن العملية قيد التقدم
     api.setMessageReaction("📱", event.messageID, () => {}, true);
 
     // جلب الصورة باستخدام القيم المدخلة
-    const response = await axios.get(`https://deku-rest-api.gleeze.com/canvas/avatarv2?id=${encodeURIComponent(id)}&bgtext=${encodeURIComponent(bgname)}&signature=${encodeURIComponent(signature)}&color=${encodeURIComponent(color)}`, { responseType: 'arraybuffer' });
+    const response = await axios.get(`https://joshweb.click/canvas/fbcoverv4?name=${encodeURIComponent(name)}&subname=${encodeURIComponent(subname)}&id=${encodeURIComponent(id)}&colorname=${encodeURIComponent(colorname)}&colorsub=${encodeURIComponent(colorsub)}`, { responseType: 'arraybuffer' });
     fs.writeFileSync(imagePath, Buffer.from(response.data, "utf-8"));
 
     // تغيير رد الفعل لإعلام المستخدم بنجاح العملية
@@ -29,7 +29,7 @@ async function avatarAlert({ api, event, args }) {
     // إرسال الصورة بعد 5 ثواني وحذفها بعد إرسالها
     setTimeout(function () {
       api.sendMessage({
-        body: "AVATAR IMAGE",
+        body: "FB COVER IMAGE",
         attachment: fs.createReadStream(imagePath)
       }, threadID, () => {
         setTimeout(() => {
@@ -46,7 +46,7 @@ async function avatarAlert({ api, event, args }) {
 export default {
   name: "اڤتار",
   author: "kaguya project",
-  description: "يرسل صورة أفاتار بناءً على id, bgname, signature, color.\nمثال: 1 Joshua 'Joshua Sy' blue",
-  aliases: ["أڤتار"],
+  description: "يرسل صورة غلاف بناءً على name, subname, id, colorname, colorsub.\nمثال: John Doe 123 blue red",
+  aliases: ["avatar"],
   execute: avatarAlert
 };
