6f8e0de1a801b7d8725b77332806e9d9b5e062ae
HUSSEINHN123 <148639702+HUSSEINHN123@users.noreply.github.com>
2025-01-05 11:58:56 +0100

    Update help.js

--- commands/Utility/help.js
+++ commands/Utility/help.js
@@ -11,34 +11,18 @@ class Help {
     this.role = "member";
     this.aliases = ["أوامر", "الاوامر"];
     this.commands = global.client.commands;
-    this.cache = {}; // Cache to store image paths
     this.tempFolder = path.join(process.cwd(), 'temp');
 
-    // مصفوفة الصور العشوائية للعرض الأولي
+    // مصفوفة الصور العشوائية
     this.randomImageUrls = [
-            "https://i.postimg.cc/59sCqJt7/photo14.jpg",
-
-      "https://i.postimg.cc/15TV2KwD/No-Name-2019-1-27-8-22-44-No-00.jpg",
-
-      "https://i.postimg.cc/q7GRJchb/No-Name-2019-1-20-9-26-17-No-00.jpg",
-
-      "https://i.postimg.cc/wTsjyYRs/CCo-FMdqe-PSGh-UYb-JEh-B7-Jej-J7-ZH2s-Ee-ZWCKtb-C5r59spqf-OHqlf-IUNDrk-OHGs78-Nslzw-Hq-XLu-Os-Lg-Ls-W-main.jpg",
-
-      "https://i.postimg.cc/nhJ9nhSL/story-2330-photo-1656070288443767046.jpg",
-
-      "https://i.postimg.cc/HxRjRY7d/1369266-650.jpg",
-
-      "https://i.postimg.cc/RVcqpzCx/kaguyasama-wa-kokurasetai-w9nm2-V-T7c.jpg",
-
-      "https://i.postimg.cc/T3f1ZHhL/FQ-Gzh-HXMAIv-PDm.jpg",
-    ];
-
-    // مصفوفة الصور المخصصة للرد عند جلب معلومات أمر معين
-    this.detailedImageUrls = [
-    "https://i.postimg.cc/jj25dynJ/thumb-350-1080006.webp",
-    "https://i.postimg.cc/d32QSBpg/thumb-350-1239849.webp",
-    "https://i.postimg.cc/J4HQQPVT/VZKKBHv.jpg",
-      "https://i.postimg.cc/wxZX3LyD/fX5iiTb.png",  // قم بتعديل الرابط حسب الحاجة
+      "https://i.postimg.cc/ncSwYctL/1198010.jpg",
+      "https://i.postimg.cc/x81SxfT1/4k-popstar-ahri-asu-lol-skin-splash-art-4k-wallpaper-pixground.jpg",
+      "https://i.postimg.cc/kG3TGwL3/4k-Rising-Legend-Ahri-Skin-League-Of-Legends-4-K-Wallpaper.jpg",
+      "https://i.postimg.cc/VsKSY5t4/foxfire-ahri-asu-lol-skin-splash-art-4k-wallpaper-pixground-768x432.jpg",
+      "https://i.postimg.cc/ZKZCFpST/o-Nh-Ocuu-QQ78g-Ylow6r-Xoyefy8-L166-G0-H1u-Ame-Mtq.jpg",
+      "https://i.postimg.cc/hvtNc2mD/undefined-Imgur.jpg",
+      "https://i.postimg.cc/WbbSYDmv/Utool-20240705-090316235.jpg",
+      "https://i.postimg.cc/264p8q36/wallpapersden-com-new-ahri-league-of-legends-1920x1080.jpg" 
     ];
   }
 
@@ -47,7 +31,7 @@ class Help {
 
     const [pageStr] = args;
     const page = parseInt(pageStr) || 1;
-    const commandsPerPage = 10; // تعديل عدد الأوامر في كل صفحة
+    const commandsPerPage = 10;
     const startIndex = (page - 1) * commandsPerPage;
     const endIndex = page * commandsPerPage;
 
@@ -56,18 +40,17 @@ class Help {
     const totalCommands = commandList.length;
 
     if (pageStr && typeof pageStr === 'string' && pageStr.toLowerCase() === 'الكل') {
-      let allCommandsMsg = "╭───────────────◊\n•——[قِـٰٚـِْ✮ِـٰٚـِْآئمِـٰٚـِْ✮ِـٰٚـِْة جِـٰٚـِْ✮ِـٰٚـِْمِـٰٚـِْ✮ِـٰٚـِْيِـٰٚـِْ✮ِـٰٚـِْعِـٰٚـِْ✮ِـٰٚـِْ آلِـٰٚـِْ✮ِـٰٚـِْأﯛ̲୭آمِـٰٚـِْ✮ِـٰٚـِْر║]——•\n";
-
-
-
-      commandList.forEach((command, index) => {
+      let allCommandsMsg = "╭───────────────◊\n•——[قائمة جميع الأوامر]——•\n";
+      
+      commandList.forEach((command) => {
         const commandName = command.name.toLowerCase();
         allCommandsMsg += `❏ الإسم : 『${commandName}』\n`;
       });
-      allCommandsMsg +=`إجِٰـِۢمِٰـِۢآلِٰـِۢيِٰـِۢ عِٰـِۢدد آلِٰـِۢأﯛ̲୭آمِٰـِۢر : ${totalCommands} أمر\n╰───────────────◊`;
+
+      allCommandsMsg += `إجمالي عدد الأوامر: ${totalCommands} أمر\n╰───────────────◊`;
       await api.sendMessage(allCommandsMsg, event.threadID);
     } else if (!isNaN(page) && page > 0 && page <= totalPages) {
-      let msg = `\n•—[قٰཻــ͒͜ـًائمـٰة أوُامـٰࢪ ڪاغــِْــٰوُيا ]—•\n`;
+      let msg = `\n•—[قــائــمــة أوامــر مــيــكــو]—•\n`;
 
       const commandsToDisplay = commandList.slice(startIndex, endIndex);
       commandsToDisplay.forEach((command, index) => {
@@ -75,8 +58,7 @@ class Help {
         msg += `[${commandNumber}] ⟻『${command.name}』\n`;
       });
 
-      msg +=`ـــــــــــــ❀✿❀✿❀✿❀✿ـــــــــــــ\nاٰلـٰ̲ـہصـٰ̲ـہفـٰ̲ـہحـٰ̲ـة : ${page}/${totalPages}:\nإجِٰـِۢمِٰـِۢآلِٰـِۢيِٰـِۢ عِٰـِۢدد آلِٰـِۢأﯛ̲୭آمِٰـِۢر : ${totalCommands} أمر\n🔖 | قم بكتابة أوامر 'رقم الصفحة' من أجل رؤية باقي الصفحات \n 🧿 | أو قم بكتابة اوامر الكل من أجل رؤية جميع الأوامر\n 📜 | رد بـ رقم الأمر من أجل مزيد من التفاصيل`;
-
+      msg += `✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏✎\nالصفحة: ${page}/${totalPages}\nإجمالي عدد الأوامر: ${totalCommands} أمر\n🔖 | اكتب 'أوامر رقم الصفحة' لرؤية الصفحات الأخرى.\n 🧿 | اكتب 'أوامر الكل' لرؤية جميع الأوامر.`;
 
       const randomImageUrl = this.randomImageUrls[Math.floor(Math.random() * this.randomImageUrls.length)];
       const tempImagePath = path.join(this.tempFolder, `random_image_${Date.now()}.jpeg`);
@@ -86,68 +68,13 @@ class Help {
         fs.writeFileSync(tempImagePath, Buffer.from(imageResponse.data));
         const attachment = fs.createReadStream(tempImagePath);
 
-        const info = await api.sendMessage({ body: msg, attachment }, event.threadID);
-
-        // Add onReply handler
-        global.client.handler.reply.set(info.messageID, {
-          author: event.senderID,
-          type: "pick",
-          name: "اوامر",
-          unsend: false,
-        });
+        await api.sendMessage({ body: msg, attachment }, event.threadID);
       } catch (error) {
         console.error("حدث خطأ: ", error);
         await api.sendMessage("❌ | حدث خطأ أثناء جلب الصورة.", event.threadID);
       }
     } else {
       await api.sendMessage("❌ | الصفحة غير موجودة.", event.threadID);
-    }
-  }
-
-  async onReply({ api, event, reply }) {
-    if (reply.type === "pick" && reply.name === "اوامر" && reply.author === event.senderID) {
-      const commandNumber = parseInt(event.body.trim());
-
-      if (isNaN(commandNumber) || commandNumber < 1 || commandNumber > this.commands.size) {
-        return api.sendMessage("❌ | رقم الأمر غير صحيح.", event.threadID);
-      }
-
-
-      const commandList = Array.from(this.commands.values());
-      const selectedCommand = commandList[commandNumber - 1];
-
-      const roleText = this.getRoleText(selectedCommand.role);
-
-        api.setMessageReaction("🎯", event.messageID, (err) => {}, true);
-
-      const message = `\n\n\n⎔━━━━〚〘${selectedCommand.name}〙〛━━━⎔\n
-👤 | ➭ المؤلف:『${selectedCommand.author}』
-🔑 | ➭ الدور:『${roleText}』
-📋 | ➭ الوصف:『${selectedCommand.description}』
-📝 | ➭ اسماء بديلة:『${selectedCommand.aliases?.join(", ") || "غير متوفرة"}』\n⎔━━━━〚〘${selectedCommand.name}〙〛━━━⎔`;
-      const detailedImageUrl = this.detailedImageUrls[Math.floor(Math.random() * this.detailedImageUrls.length)];
-      const tempImagePath = path.join(this.tempFolder, `detailed_image_${Date.now()}.jpeg`);
-
-      try {
-        const imageResponse = await axios.get(detailedImageUrl, { responseType: 'arraybuffer' });
-        fs.writeFileSync(tempImagePath, Buffer.from(imageResponse.data));
-        const attachment = fs.createReadStream(tempImagePath);
-        await api.sendMessage({ body: message, attachment }, event.threadID);
-      } catch (error) {
-        console.error("حدث خطأ: ", error);
-        await api.sendMessage("❌ | حدث خطأ أثناء جلب الصورة.", event.threadID);
-      }
-    }
-  }
-
-  getRoleText(role) {
-    switch (role) {
-      case "admin":
-        return "المشرفين";
-      case "owner":
-        return "المالك";
-      default:
-        return "الجميع";
     }
   }
 }
